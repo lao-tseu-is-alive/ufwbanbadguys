96bdbd48b90188a65e6ef2dfea9a988d45c2d4f4
CGil <lao.tseu.is.alive@gmail.com>
2018-08-03 20:25:26 +0200

    added handling of existing denied ip and whitelist handling

--- ufwbanbadguys.py
+++ ufwbanbadguys.py
@@ -1,11 +1,46 @@
-#!python3
+#!/usr/bin/python3
 import re
+import subprocess
 
 # set the path of the linux ssh auth log
 # TODO allow to pass this path as argument to the script
 authLog = "/var/log/auth.log"
+whiteList = "config/WHITELIST"
 # set the maximum number of password errors before being
-maxAllowedErrors = 5
+maxAllowedErrors = 20
+
+# allows to test if ip is in Sunrise range
+def isSunriseIP(ip):
+    a, b, c, d = [int(x) for x in ip.split('.')]
+    if a != 178: return False
+    if b != 39: return False
+    if (c < 64) or (c > 127): return False
+    return True
+
+
+# give back array of white listed ip in config file
+def getWhiteListIp(whiteListConfigFile):
+    with open(whiteListConfigFile, "r") as f:
+        return [line.rstrip() for line in f]
+
+
+# give array of already denied ip in UFW firewall
+def getFwDeniedIP():
+    deniedIp = []
+    ufwResult = subprocess.run(['ufw', 'status'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    for line in ufwResult.split('\n'):
+        line = line.strip()
+        if re.match('Anywhere\s+DENY\s+([0-9]+(?:\.[0-9]+){3})', line):
+            ipList = re.findall('[0-9]+(?:\.[0-9]+){3}', line)
+            deniedIp.append(ipList[0])
+    return deniedIp
+
+
+ipWhiteList = getWhiteListIp(whiteList)
+# print(ipWhiteList)
+# get a list of ip already blocked by UFW
+ipBlocked = getFwDeniedIP()
+# print(ipBlocked)
 ipDic = dict()
 with open(authLog) as file:
     for line in file:
@@ -16,11 +51,24 @@ with open(authLog) as file:
             ipList = re.findall('[0-9]+(?:\.[0-9]+){3}', line)
             # get the first one
             ip = ipList[0]
-
             ipDic[ip] = ipDic.get(ip, 0) + 1
-for k in sorted(ipDic, key=ipDic.get, reverse=True):
-    # for now just display the 'top bad guys ip' and a proposal of ufw command
-    # TODO I must check if the ip is already present in the UFW rules
-    if ipDic[k] > maxAllowedErrors:
-        print("{ip} : {count} ".format(ip=k, count=ipDic[k]))
-        print("ufw insert 1 deny from {ip}".format(ip=k))
+cmdToRun = ""
+cmdToLog = ""
+for ipKey in sorted(ipDic, key=ipDic.get, reverse=True):
+    if ipDic[ipKey] > maxAllowedErrors:
+        if ipKey in ipWhiteList:
+            info = ' ## THIS IP IS IN YOUR WHITE LIST ##'
+        elif isSunriseIP(ipKey):
+            info = ' ## THIS IP IS IN SUNRISE SUBNET ##'
+        else:
+            if ipKey in ipBlocked:
+                info = ' OK ip Already blocked by your UFW firewall'
+            else:
+                cmdToRun += ("ufw insert 1 deny from {ip}\n".format(ip=ipKey))
+                cmdToLog += ("grep '{ip}' /var/log/auth.log > log/badguys/auth_{ip}.log\n".format(ip=ipKey))
+                info = ' WARNING you better BAN this IP'
+        print("{ip} : {count} --> {msg}".format(ip=ipKey, count=ipDic[ipKey], msg=info))
+    
+print("\nYou can run this commands:\n")
+print(cmdToRun)
+print(cmdToLog)
